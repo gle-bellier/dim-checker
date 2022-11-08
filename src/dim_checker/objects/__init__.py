a6a3e2b0b7c32cf1c484c85ff378fe0e439966cb
gle-bellier <georges.lebellier@gmail.com>
2022-11-08 15:16:36 +0100

    Update imports in init

--- src/dim_checker/objects/__init__.py
+++ src/dim_checker/objects/__init__.py
@@ -1,3 +1,3 @@
-from dim_checker.objects.formulas import Formula
+from dim_checker.objects.formulas import Formula, VectorFormula
 from dim_checker.objects.patterns import Pattern
 from dim_checker.objects.constraints import Constraints
