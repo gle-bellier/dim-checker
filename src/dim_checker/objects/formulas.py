d06c735920c80c431028765f32cdfe9c2b4a6831
gle-bellier <georges.lebellier@gmail.com>
2022-11-08 14:00:24 +0100

    Add vector formula class to handle several inputs or outputs

--- src/dim_checker/objects/formulas.py
+++ src/dim_checker/objects/formulas.py
@@ -2,31 +2,35 @@ from typing import Tuple
 
 from dim_checker.errors.formula_errors import FormulaCharacterError, FormulaParenthesisError 
 
-class Formula:
+
+
+
+
+class VectorFormula:
     
-    def __init__(self, formula:str) -> None:
-        """Initializes formula object.
+    def __init__(self, vector_formula:str) -> None:
+        """Initializes vector formula object.
 
         Args:
-            formula (str): input or output formula.
+            formula (str): vector formula.
         """
-        self.formula = formula
-        self.dims, self.variables = self.parse_formula(formula)
+        self.vector_formula = vector_formula
+        self.dims, self.variables = self.parse_vector_formula(vector_formula)
 
     def __repr__(self) -> str:
-        """Creates string representation of the formula.
+        """Creates string representation of the vector formula.
 
         Returns:
-            str: string representation of the formula.
+            str: string representation of the vector formula.
         """
-        return f"Formula: {self.formula}."
+        return f"""Vector formula: "{self.vector_formula}"."""
 
-    def parse_formula(self, formula: str) -> Tuple[list[str], set[str]]:
-        """Parse the formula into list of the dimensions and the set 
+    def parse_vector_formula(self, vector_formula: str) -> Tuple[list[str], set[str]]:
+        """Parse the vector formula into list of the dimensions and the set 
         of variables involved in the dimensions definitions. 
 
         Args:
-            formula (str): input or output formula.
+            vector_formula (str): input or output vector formula.
 
         Raises:
             FormulaParenthesisError: error raised if any unclosed parenthesis in the formula.
@@ -46,7 +50,7 @@ class Formula:
         # initialize arithmetical expression
         exp = ""
 
-        for c in formula:
+        for c in vector_formula:
             if c == "(":
                 exp += c
                 n_par += 1
@@ -55,7 +59,7 @@ class Formula:
                 
                 # raise error if empty parenthesis
                 if exp == "(":
-                    raise FormulaParenthesisError(formula)
+                    raise FormulaParenthesisError(vector_formula)
 
                 n_par -= 1
                 exp += c
@@ -79,11 +83,57 @@ class Formula:
 
                     else:
                         # unvalid character
-                        raise FormulaCharacterError(formula, c)
+                        raise FormulaCharacterError(vector_formula, c)
 
         # raise error if there is unclosed parenthesis
         if n_par != 0:
-            raise FormulaParenthesisError(formula)
+            raise FormulaParenthesisError(vector_formula)
 
 
         return dims, variables
+
+
+
+
+class Formula:
+
+    def __init__(self, formula: str) -> None:
+        """Initializes formula object.
+
+        Args:
+            formula (str): formula string description.
+        """
+        self.formula = formula
+        self.vector_formulas = self.parse_formula(formula)
+
+    def parse_formula(self, formula: str) -> list[VectorFormula]:
+        """Parse formula into vector formulas.
+
+        Args:
+            formula (str): formula string description.
+
+        Returns:
+            list[VectorFormula]: list of VectorFormulas corresponding to 
+            each vector formulas included in formula.
+        """
+        # remove spaces
+        formula = formula.replace(" ", "")
+        # split formula to get each vector formula
+        vector_formulas = formula.split(",")
+        # create vector formula for each non empty formula
+        return [VectorFormula(vf) for vf in vector_formulas if vf!=""]
+
+
+    def __repr__(self) -> str:
+        """Creates string representation of the formula.
+
+        Returns:
+            str: string representation of the formula.
+        """
+        s = f"Formula composed of {len(self.vector_formulas)} vector formula(s): "
+        for vf in self.vector_formulas:
+            s+=f"\n -> {str(vf)}"
+        return s
+
+
+
